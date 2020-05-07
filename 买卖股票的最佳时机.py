588695e85f43eb9fc64d553baa2a619ce064e123
wshzd <w-s-h-z-d@163.com>
2020-05-07 15:41:25 +0800

    Create 买卖股票的最佳时机.py

---
+++ 买卖股票的最佳时机.py
@@ -0,0 +1,15 @@
+题目：给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+注意：你不能在买入股票前卖出股票。
+
+def maxProfit(self, prices: List[int]) -> int:
+        inf = int(1e9)
+        minprice = inf
+        maxprofit = 0
+        for price in prices:
+            maxprofit = max(price - minprice, maxprofit)
+            minprice = min(price, minprice)
+        return maxprofit
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/121-mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode-/
