8b09699e257acb7f796cb36f520995802abb34ca
wshzd <w-s-h-z-d@163.com>
2020-05-07 20:42:36 +0800

    Update 两个字符串匹配.py

--- 两个字符串匹配.py
+++ 两个字符串匹配.py
@@ -1,4 +1,5 @@
 题目：给定两个字符串str1和str2，len(str2）>len(str1)，判断str2是否包括str1中全排列的任何一个
+方案一：
 本方法是用递归实现，使用全局变量count来记录全排列一共有
 多少种。全局变量temp来记录排列中的状态。每次确定一个元素position变量记录当前排列元素的位置，然后在剩余元素s[s.index(i)+1:]+s[: s.index(i)]中继续进行全排列，直到剩余
 元素个数为零，输出temp；然后回退temp// temp = temp[:position]，直到输出所有结果！
@@ -25,3 +26,17 @@ order(in_put, 0)
 print(count)
 
 原文链接：https://blog.csdn.net/RobotM/article/details/105079739
+                        
+                        
+                        
+                                           
+方案二：
+def permutation(s,i):
+    if i == len(s):
+        print(s)
+    else:
+        for j in range(i,len(s)):
+            s[j],s[i] = s[i],s[j]
+            permutation(s,i+1)
+            s[j],s[i] = s[i],s[j]
+原文链接：https://blog.csdn.net/m0_37422289/article/details/80570726
