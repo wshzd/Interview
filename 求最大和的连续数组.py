602e4b7e56ac9d7e208934224db5340f05f0047c
wshzd <w-s-h-z-d@163.com>
2020-05-07 14:38:05 +0800

    Update 求最大和的连续数组.py

--- 求最大和的连续数组.py
+++ 求最大和的连续数组.py
@@ -1,5 +1,5 @@
 题目：给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和
-def maxSubArray(self, nums: List[int]) -> int:
+def maxSubArray(nums: List[int]) -> int:
         if len(nums) == 0:
             return 0
         if len(nums) == 1:
